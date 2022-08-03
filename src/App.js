5bbec603f015503a298df29ee2a64b4e8edc0305
Anas-Ali-coder <63842551+Anas-Ali-coder@users.noreply.github.com>
2022-08-03 12:54:31 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./routes/About";
+import Cart from "./routes/Cart";
+import Home from "./routes/Home";
+import NavBar from "./routes/NavBar";
+import SignIn from "./routes/SignIn";
+import SignOut from "./routes/SignOut";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>hi</h2>
+      <NavBar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
   );
 }
 
--- src/routes/NavBar.js
+++ src/routes/NavBar.js
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/signin">SignIn</Link>
-        </li>
-        <li>
-          <Link to="/signout">SignOut</Link>
-        </li>
-        <li>
-          <Link to="/signout">SignOut</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/signin">SignIn</Link>
+          </li>
+          <li>
+            <Link to="/signout">SignOut</Link>
+          </li>
+          <li>
+            <Link to="/signout">SignOut</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
